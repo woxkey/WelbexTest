491b00b3e6f368261105bd65e37a900356db41dc
Alen <bolatovalen@gmail.com>
2023-06-15 16:47:16 +0600

    added t category

--- src/screens/MapScreen.tsx
+++ src/screens/MapScreen.tsx
@@ -2,24 +2,27 @@ import {View} from 'react-native';
 import MapView, {Marker} from "react-native-maps";
 import {IData} from "./HomeScreen";
 import React, {FC} from "react";
+import {useTranslation} from "react-i18next";
 
 interface MapScreenProps {
     vehicles: IData[];
 }
 
 const MapScreen: FC<MapScreenProps> = ({vehicles}) => {
+    const {t} = useTranslation();
 
     return (<View style={{flex: 1, paddingTop: 10}}>
-            <MapView style={{height: "100%", width: "100%"}}>
-                {vehicles.map((vehicle: IData) => {
-                    return (<Marker key={vehicle.vehicleId}
-                                    pinColor={vehicle.category === "cargo" ? "yellow" : vehicle.category === "special" ? "red" : "blue"}
-                                    title={vehicle.driver} coordinate={{
-                            latitude: vehicle.coordinate.latitude, longitude: vehicle.coordinate.longitude,
-                        }}/>)
-                })}
-            </MapView>
-        </View>);
+        <MapView style={{height: "100%", width: "100%"}}>
+            {vehicles.map((vehicle: IData) => {
+                return (<Marker key={vehicle.vehicleId}
+                                pinColor={vehicle.category === "cargo" ? "yellow" : vehicle.category === "special" ? "red" : "blue"}
+                                title={t(vehicle.category) || vehicle.category} coordinate={{
+                    latitude: vehicle.coordinate.latitude, longitude: vehicle.coordinate.longitude,
+                }}/>)
+            })}
+
+        </MapView>
+    </View>);
 };
 
 export default MapScreen;
